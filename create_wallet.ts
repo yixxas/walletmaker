acad7e62d1d10cdddc7b02343ef58faf20a730ff
yixxas <limbangjin0@gmail.com>
2022-03-23 04:57:56 +0800

    First commit

---
+++ create_wallet.ts
@@ -0,0 +1,48 @@
+import * as ethers from 'ethers';
+import * as readline from 'readline';
+import * as fs from 'fs';
+import { stdin as input, stdout as output } from 'process';
+let assert = require('assert');
+
+
+let callback = function(progress: number){
+    console.log("Encrypting: " + progress * 100 + "% complete");   
+}
+
+let user_input = function(query: string) {
+    const rl = readline.createInterface({input, output});
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }));
+}
+
+let create_wallet = async function(){
+
+    let wallet = ethers.Wallet.createRandom();
+
+    let options = {
+        "scrypt": {"N": 2**14, "r": 224, "p": 4}
+    };
+
+    let password1: string = await user_input("Enter password: ") as string;
+    let password2: string = await user_input("Confirm password: ") as string;
+
+    assert(password1 === password2);
+
+    let encryptPromise = await wallet.encrypt(password1,options,callback);
+
+    fs.writeFile("wallet.json", encryptPromise, (error) => {
+        if(error){
+            return console.log(error);
+        }
+        console.log("Wallet successfully created!");
+    });
+
+}
+
+function main(){
+    create_wallet();
+}
+
+main();
